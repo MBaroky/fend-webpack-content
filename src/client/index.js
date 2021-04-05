10666a147ef4d84ccfa4d206f9cb3ff71611b980
MBaroky <m.baroky@gmail.com>
2021-04-06 01:48:35 +0200

    some fun at the end

--- src/client/index.js
+++ src/client/index.js
@@ -4,3 +4,9 @@ import { handleSubmit } from './js/formHandler'
 console.log(checkForName);
 
 alert("I EXIST")
+alert("I DO REALLY")
+alert("WHY YOU DON'T CARE?")
+alert("ARE YOU GONNA JUST CLICK OK ALL DAY?")
+alert("YOU'RE UNBELIEVABLE !!")
+alert("YOU SONOFA @#@@#$@%$^")
+alert("AM OUT'A HERE")
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const WebPackBundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     entry: './src/client/index.js',
@@ -19,6 +21,14 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            dry:true,
+            verbose:true
+        }),
+        new WebPackBundleAnalyzer({
+            analyzerMode:'static',
+            reportFilename:'wassapp!.html'
         })
     ]
 }
